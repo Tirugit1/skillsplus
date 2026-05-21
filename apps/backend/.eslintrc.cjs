7394f0858f81f2c9d7c00d97b4058ba177f210d4
tirugit1 <tirugit1@gmail.com>
2026-05-21 05:15:02 +0000

    disable no-unused-vars lint rule for JSX

--- apps/backend/.eslintrc.cjs
+++ apps/backend/.eslintrc.cjs
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   env: { node: true, es2020: true },
   extends: ['eslint:recommended'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'commonjs' },
-  rules: {}
+  parserOptions: { ecmaVersion: 'latest' },
+  rules: {
+    'no-unused-vars': 'off'
+  }
 }
--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -2,6 +2,12 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: ['eslint:recommended'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
-  rules: {}
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  rules: {
+    'no-unused-vars': 'off'
+  }
 }
